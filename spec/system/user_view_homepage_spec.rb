8f1895150cc420a6129112e5bbaed8b6ec9c7ee9
Thalis Freitas <ofthalis@gmail.com>
2022-09-15 14:57:59 -0300

    Visitante vê galpões na tela inicial

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -3,6 +3,32 @@ require 'rails_helper'
 describe 'Usuário visita tela inicial' do
   it 'e vê o nome da app' do
     visit ('/')
+
     expect(page).to have_content('Galpões & Estoque')
+  end
+
+  it 'e vê os galpões cadastrados' do 
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceió', code: 'MCZ', city: 'Maceió', area: 50_000)
+
+    visit('/')
+
+    expect(page).not_to have_content('Não existem galpões cadastrados')
+
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m²')
+
+    expect(page).to have_content('Maceió')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceió')
+    expect(page).to have_content('50000 m²')
+  end
+
+  it 'e não existem galpões cadastrados' do 
+    visit('/')
+
+    expect(page).to have_content('Não existem galpões cadastrados')
   end
 end
